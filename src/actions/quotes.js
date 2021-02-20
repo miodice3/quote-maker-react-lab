b0c5533751196083a6dc549e49010f22b7e94f29
Michael Iodice <miodice3@gmail.com>
2021-02-20 12:37:48 -0700

    commit tree d7e5f16bc4c6e6edb5054179108d0afab09af2bf

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,29 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+    return {
+        type: 'ADD_QUOTE',
+        quote
+    }
+}
+
+export const removeQuote = Integer => {
+    // console.log("REMOVE ACTION", Integer)
+    return {
+        type: 'REMOVE_QUOTE',
+        quoteId: Integer
+    }
+}
+
+export const upvoteQuote = Integer => {
+    return {
+        type: 'UPVOTE_QUOTE',
+        quoteId: Integer
+    }
+}
+
+export const downvoteQuote = Integer => {
+    return {
+        type: 'DOWNVOTE_QUOTE',
+        quoteId: Integer
+    }
+}
